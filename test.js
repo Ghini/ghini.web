201ab8bfe6015bcbe1a9d1ae8e40ac906580d94c
Mario Frasca <mario@anche.no>
2017-02-10 10:54:46 -0500

    not yet testing the server

--- test.js
+++ test.js
@@ -1,5 +1,24 @@
+// This file is part of ghini.web
+// http://github.com/Ghini/ghini.web
+//
+// ghini.web is free software: you can redistribute ghini.web and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+//
+// ghini.web is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with ghini.web.  If not, see <http://www.gnu.org/licenses/>.
+//
+// this is the remote server testing code
+//
 // 小米; Xiǎomǐ
 // 华为; Huáwéi
+
 var vows = require('vows'),
     assert = require('assert');
 

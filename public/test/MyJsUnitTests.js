a144047b107bdc130d10d9a8df659d9e16d248b7
Mario Frasca <mario@anche.no>
2017-02-10 10:55:17 -0500

    testing parts of the client

---
+++ public/test/MyJsUnitTests.js
@@ -0,0 +1,45 @@
+QUnit.test('Object.values', function (assert) {
+    var l;
+    l = {a: 1, b:2};
+    assert.deepEqual(Object.values(l), [1, 2]);
+    l = [1, 2];
+    assert.deepEqual(Object.values(l), [1, 2]);
+});
+
+QUnit.test('String.formatU', function (assert) {
+    assert.equal("abc.{0}-{1}".formatU([5, 6]), 'abc.5-6');
+    assert.equal("abc.{key}-{key}".formatU({key: 5}), 'abc.5-5');
+});
+
+QUnit.test('String.formatU-decimal_number', function (assert) {
+    assert.equal("abc.{0}-{1}".formatU([5.1, 6]), 'abc.5.1-6', 'one decimal');
+    assert.equal("abc.{0}-{1}".formatU([5.11, 6]), 'abc.5.11-6', '2 decimals');
+    assert.equal("abc.{0}-{1}".formatU([5.111, 6]), 'abc.5.111-6', '3 decimals');
+    assert.equal("abc.{0}-{1}".formatU([5.1111, 6]), 'abc.5.1111-6', '4 decimals');
+    assert.equal("abc.{0}-{1}".formatU([5.11111, 6]), 'abc.5.11111-6', '5 decimals');
+    assert.equal("abc.{0}-{1}".formatU([5.111111, 6]), 'abc.5.111111-6', '6 decimals');
+    assert.equal("abc.{0}-{1}".formatU([5.1111111, 6]), 'abc.5.111111-6', '7 decimals - trimmed');
+    assert.equal("abc.{0}-{1}".formatU([0.000006, 6]), 'abc.0.000006-6', '6 decimals - ok');
+    assert.equal("abc.{0}-{1}".formatU([0.0000066, 6]), 'abc.0.000007-6', '7 decimals - rounded');
+});
+
+QUnit.test('parse_hash-garden', function(assert) {
+    assert.deepEqual(parse_hash('#garden=Cuchubo'), {garden: 'Cuchubo'});
+    assert.deepEqual(parse_hash('#garden=cuchubo'), {garden: 'cuchubo'});
+});
+
+QUnit.test('parse_hash-map', function(assert) {
+    assert.deepEqual(parse_hash('#map=1/2/3'), {map: {zoom:1, lat:2, lon:3}});
+    assert.deepEqual(parse_hash('#map=1/2.1/3.2'), {map: {zoom:1, lat:2.1, lon:3.2}});
+});
+
+QUnit.test('parse_hash-garden-map', function(assert) {
+    var parsed = parse_hash('#garden=Cuchubo;map=1/2/3');
+    assert.deepEqual(parsed['garden'], 'Cuchubo');
+    assert.deepEqual(parsed['map'], {zoom:1, lat:2, lon:3});
+});
+
+QUnit.test('parse_hash-ignore', function(assert) {
+    assert.deepEqual(parse_hash('#maps=1/2/3'), {});
+    assert.deepEqual(parse_hash('#mapu=1/2.1/3.2'), {});
+});

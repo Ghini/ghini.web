58ecaa61f2b6e46726fc40f15e82fc6a62289469
mfrasca <mario@anche.no>
2018-07-16 16:20:28 -0500

    license and cleaning up

--- scripts/addphonetic.py
+++ scripts/addphonetic.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+# Copyright (c) 2015-2018 Mario Frasca <mario@anche.no>
+#
+# This file is part of ghini.web.
+#
+# ghini.web is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# ghini.web is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with ghini.web. If not, see <http://www.gnu.org/licenses/>.
+
 import json
 import pymongo
 client = pymongo.MongoClient('localhost:27017')
